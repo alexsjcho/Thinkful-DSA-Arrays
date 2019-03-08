37cd9a46c09de4f77dd45ed75ed48c512654f30b
Alex Cho <alexsjcho@gmail.com>
2019-03-07 19:05:06 -0800

    attempted maxSumInArray

---
+++ maxSumInArray.js
@@ -0,0 +1,25 @@
+/* 
+Problem Description:
+Max sum in the array
+You are given an array containing positive and negative integers. Write an algorithm which will find the largest sum in a continuous sequence.
+
+
+Input: [4, 6, -3, 5, -2, 1]
+Output: 12
+
+*/
+
+/* 
+Resources:
+Kadane's Algorithm to Maximum Sum Subarray Problem
+https://www.youtube.com/watch?v=86CQq3pKSUw
+Maximum Sum Sub Array
+https://www.youtube.com/watch?v=ohHWQf1HDfU
+*/
+
+function maxSumArray(array) {
+  maxCurrent = maxGlobal = array[0];
+  for (let i = 0; i < array.length; ) {}
+}
+
+maxSumArray([4, 6, -3, 5, -2, 1]);
