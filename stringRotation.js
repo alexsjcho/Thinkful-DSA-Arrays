878a0932b275cfee8b5664046d07a8b935e2e603
Alex Cho <alexsjcho@gmail.com>
2019-03-07 19:26:50 -0800

    attempted stringRotation problem

---
+++ stringRotation.js
@@ -0,0 +1,32 @@
+/* 
+Given 2 strings, str1 and str2, write a program that checks if str2 is a rotation of str1.
+
+Input: amazon, azonma
+
+Output: False
+
+Input: amazon, azonam
+
+Output: true
+
+*/
+
+let str1 = "junk";
+let str2 = "knuj";
+
+function isSubstring(str1, str2){
+    if (str2 in str1){
+        return True
+    } else{
+        return False
+    }
+}
+
+function rotateString (str1, str2){
+    if(str1.length !=== str2.length){
+        return false;
+    } else {
+        str1 += str1
+        return isSubstring(str1, str2)
+    }
+}
